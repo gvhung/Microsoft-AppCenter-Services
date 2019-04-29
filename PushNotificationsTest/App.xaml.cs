7cde21e56f3fdcfdae63f2f0afda44880720c10f
mnjpkulkarni@gmail.com <mnjpkulkarni@gmail.com>
2019-04-29 16:45:21 -0400

    Initial check-in of module PushNotificationsTest

---
+++ PushNotificationsTest/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Push;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace PushNotificationsTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+
+            Push.SetEnabledAsync(true);
+
+            // This should come before AppCenter.Start() is called
+            // Avoid duplicate event registration:
+            if (!AppCenter.Configured)
+            {
+                Push.PushNotificationReceived += (sender, e) =>
+                {
+                    // Add the notification message and title to the message
+                    var summary = $"Push notification received:" +
+                                        $"\n\tNotification title: {e.Title}" +
+                                        $"\n\tMessage: {e.Message}";
+
+                    // If there is custom data associated with the notification,
+                    // print the entries
+                    if (e.CustomData != null)
+                    {
+                        summary += "\n\tCustom data:\n";
+                        foreach (var key in e.CustomData.Keys)
+                        {
+                            summary += $"\t\t{key} : {e.CustomData[key]}\n";
+                        }
+                    }
+
+                    // Send the notification summary to debug output
+                    System.Diagnostics.Debug.WriteLine(summary);
+                };
+            }
+
+            AppCenter.Start("ae7ad483-1b5a-4711-9bad-9d840823c7f7", typeof(Push), typeof(Analytics), typeof(Crashes));
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ PushNotificationsTest/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Push;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+
+namespace PushNotificationsTest
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            check();
+
+            CustomProperties customProperties = new CustomProperties();
+            customProperties.Set("","");
+            AppCenter.SetCustomProperties(customProperties);
+        }
+
+        async void check()
+        {
+            bool appcenterIsEnabled = await AppCenter.IsEnabledAsync();
+            System.Console.Write(appcenterIsEnabled);
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Analytics.TrackEvent("Button Clicked");
+            try
+            {
+                throw new System.ArgumentException();
+            }
+            catch (Exception exception)
+            {
+                Crashes.TrackError(exception);
+            }
+        }
+    }
+}
